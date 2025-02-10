ae868a9f1efbc158569d37ebf0d8cfed732db232
Wins444 <winsking04yt@gmail.com>
2025-02-10 11:04:23 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const suits = ["♥", "♦", "♣", "♠"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+
+function generateRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const cardElement = document.getElementById("card");
+
+  cardElement.querySelector(".top-suit").textContent = randomSuit;
+  cardElement.querySelector(".number").textContent = randomValue;
+  cardElement.querySelector(".bottom-suit").textContent = randomSuit;
+
+  const color = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+  cardElement.style.color = color;
+}
+
+document.getElementById("newCard").addEventListener("click", generateRandomCard);
+
+document.getElementById("width").addEventListener("input", (e) => {
+  document.querySelector(".card").style.width = e.target.value + "px";
+});
+
+document.getElementById("height").addEventListener("input", (e) => {
+  document.querySelector(".card").style.height = e.target.value + "px";
+});
+
+window.onload = generateRandomCard;
